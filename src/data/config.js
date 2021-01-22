4d6b5954f4ac7efdc14805e6ee65422b0ff82818
Sujatha Iyer <sujathaiyer124@gmail.com>
2021-01-22 17:05:57 +0000

    My first commit

---
+++ src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Sujatha Iyer",
+  greeting: "Hey 👋 welcome to my page ",
+  greetingDescription: "I'm Sujatha Iyer and I'm a Software Engineer!",
+  githubUrl: "https://github.com/sujathaiyer124",
+  linkedinUrl: "https://www.linkedin.com/in/sujatha-iyer-17a5aa1a3",
+  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+};
